9c67c29188d099bf0103bb20e4734d86ad44ff4e
StaxGit <lario.free@gmail.com>
2020-04-20 22:41:53 +0200

    Add and configure put route (update)

--- routes/api.js
+++ routes/api.js
@@ -25,6 +25,7 @@ module.exports = function (app) {
     .get(function (req, res){
       let project = req.params.project;
       let conditions = {name: project};
+      // BUGS OUT IF PROJECT EMPTY! FIX!
       Project.findOne(conditions).populate("issues").exec(function (err, foundProject) { //Populate required - returns only individual issues._ids otherwise
         if (err || !foundProject) {
           console.log(err);
@@ -78,8 +79,42 @@ module.exports = function (app) {
     })
     
     .put(function (req, res){
-      var project = req.params.project;
-      
+
+      //Default to "" if not provided
+      const _id = req.body._id || "";
+      const issue_title = req.body.issue_title || "";
+      const issue_text = req.body.issue_text || "";
+      const created_by = req.body.created_by || "";
+      const assigned_to = req.body.assigned_to || "";
+      const status_text = req.body.status_text || "";
+      const open = req.body.open || "";
+
+      if (!_id) {
+        return res.json("No ID entered!");
+      }
+
+      if (!issue_title && !issue_text && !created_by && !assigned_to && !status_text && !open) {
+        res.json("No updated field sent!");
+      }
+
+      let updateObject = {};
+      //Check wether the field was updated (if not, it will be an empty string)
+      if (issue_title) {updateObject.issue_title = issue_title};
+      if (issue_text) {updateObject.issue_text = issue_text};
+      if (created_by) {updateObject.created_by = created_by};
+      if (assigned_to) {updateObject.assigned_to = assigned_to};
+      if (status_text) {updateObject.status_text = status_text};
+      if (open) {updateObject.open = false} else {updateObject.open = true};
+
+      //Find the issue by id and update it with updated fields from updateObject
+      Issue.findByIdAndUpdate(req.body._id, updateObject, {new: true}, function(err, updatedIssue) {
+        if (err) {
+          res.json("Could not update " + req.body._id)
+        } else {
+          updatedIssue.updated_on = new Date();
+          res.json("Successfully updated!");
+        }
+      })
     })
     
     .delete(function (req, res){
